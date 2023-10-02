09b9993a516384755ba39c7b46cb7330596d00cd
elena bobina <putnyk19@mail.ru>
2023-10-02 17:17:06 +0500

    26

---
+++ Task026/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int SetNamber(string message)
+{
+    System.Console.Write($"Enter number {message}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+string Natural(int N)
+{
+    if (N >= 1) return $"{N} " + Natural(N - 1);
+    else return String.Empty;
+}
+
+int N = SetNamber("N");
+System.Console.WriteLine(Natural(N));
