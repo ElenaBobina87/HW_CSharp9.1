9d55f46d7e0a58fbc7a2d9ea6cd5a52b35bd88f8
elena bobina <putnyk19@mail.ru>
2023-10-02 18:02:48 +0500

    27 and 28

---
+++ Task027/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SetNamber(string message)
+{
+    System.Console.Write($"Enter number {message}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int GetSum(int start, int end, int sum = 0)
+{
+    if (start == end) return sum + end;
+    else return sum = start + GetSum(start + 1, end);
+}
+
+int M = SetNamber("M");
+int N = SetNamber("N");
+System.Console.WriteLine(GetSum(M, N));
---
+++ Task028/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int SetNamber(string message)
+{
+    System.Console.Write($"Enter number {message}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int Akerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m != 0 && n == 0) return Akerman(m - 1, 1);
+        else return Akerman(m - 1, Akerman(m, n - 1));
+}
+
+int m = SetNamber("m");
+int n = SetNamber("n");
+System.Console.WriteLine(Akerman(m, n));
